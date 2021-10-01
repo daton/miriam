65f77e2c8dcc1b9f7bb79ce2890e6977000af173
daton <rapidclimate@gmail.com>
2021-10-01 12:33:49 -0500

    Initial commit

---
+++ src/test/java/com/example/miriam/MiriamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miriam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiriamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
